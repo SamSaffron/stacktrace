cc708016d1925b6e340df81cb84f75005a3947ed
Sam Saffron <sam.saffron@gmail.com>
2012-06-11 18:02:27 +1000

    working on range stuff

--- ext/stacktrace/stacktrace.c
+++ ext/stacktrace/stacktrace.c
@@ -1,3 +1,5 @@
+// much of this is based off reading MRI source code
+
 #include <ruby.h>
 #include <vm_core.h>
 #include <iseq.h>
@@ -6,27 +8,69 @@
 #endif
 
 
-static VALUE stacktrace(rb_thread_t *th)
+static VALUE stacktrace(int argc, VALUE* argv, rb_thread_t *th)
 {
   VALUE ary = rb_ary_new();
   
+  int start = 0;
+  int finish = -1;
+  int stack_size = 0;
   const rb_control_frame_t *limit_cfp = th->cfp;
+  const rb_control_frame_t *initial_cfp = th->cfp;
   const rb_control_frame_t *cfp = (void *)(th->stack + th->stack_size);
+  VALUE file = Qnil;
+  int line = 0;
   
+  if (argc > 0) {
+    start = NUM2INT(argv[0]);
+  }
+
+  if (argc > 1) {
+    finish = NUM2INT(argv[1]);
+  } 
+
   cfp -= 2;
+  limit_cfp -= 1;
+  stack_size = cfp - initial_cfp;
+
+  if (finish < 0) {
+    finish += 1;
+    finish = stack_size + finish;
+  }
+
+  if (start < 0) {
+    start += 1; 
+    start = stack_size + start; 
+  }
   
-  limit_cfp = RUBY_VM_NEXT_CONTROL_FRAME(limit_cfp);
+  
+  //if (cfp > (limit_cfp + finish + 1)) {
+  //   cfp = initial_cfp + finish + 1; 
+  //} else {
+  //  return ary; 
+  //}
+  
+  //if (cfp > limit_cfp + start) {
+   // limit_cfp = initial_cfp + start;
+  //} else {
+  //  return ary;
+  //}
+
 
   while (cfp > limit_cfp) {
+    VALUE hash = 0; 
 	  if (cfp->iseq != 0) {
 	      if (cfp->pc != 0) {
 		      rb_iseq_t *iseq = cfp->iseq;
           if (cfp->me) {
             rb_ary_push(ary, cfp->me->klass);
           } else if (iseq->defined_method_id) {
-            VALUE hash = rb_hash_new();
+            hash = rb_hash_new();
+            line = rb_vm_get_sourceline(cfp);
             rb_hash_aset(hash, ID2SYM(rb_intern("klass")), iseq->klass); 
             rb_hash_aset(hash, ID2SYM(rb_intern("method")), iseq->name); 
+            rb_hash_aset(hash, ID2SYM(rb_intern("filename")), iseq->filename); 
+            rb_hash_aset(hash, ID2SYM(rb_intern("linenumber")), INT2FIX(line)); 
             rb_ary_push(ary,hash);
           }
 	      }
@@ -34,15 +78,14 @@ static VALUE stacktrace(rb_thread_t *th)
     else if (RUBYVM_CFUNC_FRAME_P(cfp)) {
          ID id;
          extern VALUE ruby_engine_name;
- 
-         //if (NIL_P(file)) file = ruby_engine_name;
+         if (NIL_P(file)) file = ruby_engine_name;
          if (cfp->me->def)
            id = cfp->me->def->original_id;
          else
            id = cfp->me->called_id;
          if (id != ID_ALLOCATOR) {
 
-            VALUE hash = rb_hash_new();
+            hash = rb_hash_new();
             rb_hash_aset(hash, ID2SYM(rb_intern("klass")), cfp->me->klass); 
             rb_hash_aset(hash, ID2SYM(rb_intern("method")), rb_id2str(id)); 
             rb_ary_push(ary,hash);
@@ -55,12 +98,12 @@ static VALUE stacktrace(rb_thread_t *th)
   return rb_ary_reverse(ary);
 }
 
-static VALUE rb_st_kernel_stacktrace(void)
+static VALUE rb_st_kernel_stacktrace(int argc, VALUE* argv)
 {
-  return stacktrace(GET_THREAD());
+  return stacktrace(argc, argv, GET_THREAD());
 }
 
-static VALUE rb_st_thread_stacktrace(VALUE thval)
+static VALUE rb_st_thread_stacktrace(int argc, VALUE* argv, VALUE thval)
 {
   rb_thread_t *th = (rb_thread_t *)RTYPEDDATA_DATA(thval);
 
@@ -74,11 +117,11 @@ static VALUE rb_st_thread_stacktrace(VALUE thval)
      return Qnil;
   }
 
-  return stacktrace(th);
+  return stacktrace(argc, argv, th);
 }
 
 void Init_stacktrace()
 {
-    rb_define_global_function("stacktrace", rb_st_kernel_stacktrace, 0);
-    rb_define_method(rb_cThread, "stacktrace", rb_st_thread_stacktrace, 0);
+    rb_define_global_function("stacktrace", rb_st_kernel_stacktrace, -1);
+    rb_define_method(rb_cThread, "stacktrace", rb_st_thread_stacktrace, -1);
 }
--- test/test_stacktrace.rb
+++ test/test_stacktrace.rb
@@ -39,6 +39,39 @@ class TestStacktrace < Test::Unit::TestCase
     assert_equal frame[:klass], Array
   end
 
+  def test_filename
+    name = stacktrace[1][:filename]
+    assert_equal name, __FILE__
+  end
+
+  def test_linenumber 
+    line = stacktrace[1][:linenumber]
+    assert_equal __LINE__ - 1, line
+  end
+
+  def test_skipping
+    assert_equal stacktrace(1)[0], stacktrace[(1..-1)][0]
+  end
+
+  def test_simple_range
+    #assert_equal stacktrace(1,2), stacktrace[(1..2)]
+  end
+
+  def test_invalid_range
+    #assert_equal stacktrace(1,-10000), []
+  end
+
+  def test_negative_start
+    p stacktrace(-2)
+    p stacktrace(-8)
+    p stacktrace(-8).length
+    p stacktrace(-11)
+    p stacktrace(-11).length
+    assert_equal stacktrace(-1), stacktrace[-1]
+  end
+
+  private 
+
   # stacktrace is MUCH faster
   #def test_demo_bench
   #  Benchmark.bm(7) do |b|
